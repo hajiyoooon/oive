34515edcbc1162f42095712a248b8ea64998987d
hajiyoooon <starriest30@gmail.com>
2020-02-24 15:35:35 +0900

    first commit

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -2,6 +2,8 @@ package dao;
 
 import java.util.HashMap;
 
+import javax.naming.spi.ResolveResult;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -20,6 +22,14 @@ public class UserDAO {
 		map.put("password", password);
 		UserVO vo = session.selectOne(statement, map);
 		System.out.println(vo);
-
+	}
+	
+	public boolean insert(UserVO vo) {
+		boolean result = false;
+		String statement = "insertUser";
+		if(session.insert(statement, vo) == 1) {
+			result = true;
+		}
+		return result;
 	}
 }
--- src/main/java/my/spring/oive/HomeController.java
+++ src/main/java/my/spring/oive/HomeController.java
@@ -1,18 +1,12 @@
 package my.spring.oive;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import dao.UserDAO;
+import vo.UserVO;
 
 /**
  * Handles requests for the application home page.
@@ -25,6 +19,13 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
 		dao.selectOne("admin", "1234");
+		
+		return "example";
+	}
+	
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public String register(UserVO vo) {
+		System.out.println(dao.insert(vo));
 		
 		return "example";
 	}
