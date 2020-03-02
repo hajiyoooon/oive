573ed3e9d9c2edf326f0854ef632e750ff68681d
ds3ird <ee249@hanmail.net>
2020-03-02 20:16:17 +0900

    .

--- src/main/java/my/spring/oive/HomeController3.java
+++ src/main/java/my/spring/oive/HomeController3.java
@@ -1,14 +1,15 @@
 package my.spring.oive;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import dao.UserDAO;
-import service.UserService;
-import vo.UserVO;
 
 /**
  * Handles requests for the application home page.
@@ -17,47 +18,36 @@ import vo.UserVO;
 public class HomeController3 {
 	@Autowired
 	UserDAO dao;
-	
-	@Autowired
-	UserService service;
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
-		return "example";
-	}
-	
-	//TODO : �굹以묒뿉 POST 諛⑹떇�쑝濡� 諛붽퓭二쇨린
-	@RequestMapping(value = "/join", method = RequestMethod.GET)
-	public String register() {
-		
-		return "join";
-	}
-	
-	@RequestMapping(value = "/self_introduce/list", method = RequestMethod.GET)
-	public String list() {
 
-		
-		return "list";
-	}
-	
-	@RequestMapping(value = "/profile", method = RequestMethod.GET)
-	public String profile() {
-		
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
 		return "profile";
 	}
 	@RequestMapping(value = "/form/{category}", method = RequestMethod.GET)
-	public String form(@PathVariable String category) { 
+	public String form(@PathVariable String category) {
 		return "form/"+ category +"_form";
 	}
-	
+
 	@RequestMapping(value = "/self_introduce/write/{self_introduce_id}", method = RequestMethod.GET)
 	public String write(UserVO vo) {
 		return "write";
 	}
-	
+
 	@RequestMapping(value = "/base", method = RequestMethod.GET)
 	public String base() {
 		return "base";
 	}
-	
+
 }
