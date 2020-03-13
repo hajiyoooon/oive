8551637e58afd4930303889c9f936e87ae28322c
hajiyoooon <starriest30@gmail.com>
2020-03-13 09:21:06 +0900

    VO 수정

--- src/main/java/my/spring/oive/ProfileController_temp.java
+++ src/main/java/my/spring/oive/ProfileController_temp.java
@@ -5,13 +5,11 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
